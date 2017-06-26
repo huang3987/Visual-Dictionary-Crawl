82fa147eebe107da5bec5bef24a946eae6ddea4a
huang3987 <huang3987@126.com>
2017-06-26 20:33:57 +0800

    comment the print function

--- GetWordFromVD.py
+++ GetWordFromVD.py
@@ -44,7 +44,7 @@ section = progress
 page_num = 7*section-6
 page_end = 7*section
 
-print "page_num = " + str(page_num)
+#print "page_num = " + str(page_num)
 
 while True:
 
@@ -52,15 +52,15 @@ while True:
 
     html_file_text = html_file.read()
 
-    #print html_file_text
+    ##print html_file_text
 
-    print "local_current_URL = " + local_current_URL
+    #print "local_current_URL = " + local_current_URL
 
     local_current_URL = local_current_URL.split('/')
 
     local_current_URL.pop()
 
-    #print "local_current_URL = " + local_current_URL
+    ##print "local_current_URL = " + local_current_URL
 
     
 
@@ -79,8 +79,8 @@ while True:
     
     decker02 = "::".join(decker01)
     
-    print "deck = " + decker02
-    print "decker_name=" + decker_name
+    #print "deck = " + decker02
+    #print "decker_name=" + decker_name
     
     with open(decker_name, 'w') as decker_file:        
         decker_file.write(decker02)
@@ -88,8 +88,8 @@ while True:
     #we will find out the pciture path below
        
     pic_path = re.findall(r'<div><img src=\"(.*?).jpg', html_file_text, re.S)
-    print '*'*40
-    #print pic_path
+    #print '*'*40
+    ##print pic_path
     
        
     pic_path_temp = local_current_URL[:]
@@ -98,7 +98,7 @@ while True:
     
     temp_temp = []
     
-    #print temp
+    ##print temp
     for n in range(len(temp_pic)):
         if temp_pic[n]=='..':
             pic_path_temp.pop()    
@@ -109,44 +109,44 @@ while True:
     temp_pic[-1] = temp_pic[-1] + ".jpg"
     pic_path_temp.extend(temp_pic)
     pic_path = "/".join(pic_path_temp)
-    print pic_path
+    #print pic_path
     
-    print '*'*40
+    #print '*'*40
     
-    print "local_current_URL = "
-    print local_current_URL
+    #print "local_current_URL = "
+    #print local_current_URL
     
     #we find out the text below
 
     content = []
     noun = ""
     sentences = re.findall(r'descript(.*?)v>', html_file_text, re.S)
-    print '*'*40
-    print sentences
+    #print '*'*40
+    #print sentences
     for n in range(len(sentences)):
         line = [] 
 
         #sentence = re.findall(r'h4(.*?)/di', sentences[n], re.S)
-        #print sentence
+        ##print sentence
         sentence = re.sub(r'><', '', sentences[n], re.S)
-        print "1"
-        print sentence
+        #print "1"
+        #print sentence
         sentence = re.sub(r'\&\#8217', "'", sentence, re.S)
-        print "2"
-        print sentence
+        #print "2"
+        #print sentence
         sentence = ' '.join(sentence.split())
-        print "3"
-        print sentence
+        #print "3"
+        #print sentence
         sentence = re.sub(r'> <', '', sentence, re.S)
-        print "4"
-        print sentence
+        #print "4"
+        #print sentence
         sentence = re.findall(r'>(.*?)<', sentence, re.S)
-        print "5"
-        print sentence
+        #print "5"
+        #print sentence
         if len(sentence)==3:
             del sentence[-1]
-            print "6"
-            print sentence
+            #print "6"
+            #print sentence
         #del sentence[0]
         noun = sentence[0]
 
@@ -160,17 +160,17 @@ while True:
         line = '\\'.join(line)
         content.append(line)
 
-        #print '*'*40
+        ##print '*'*40
 
         
-    for n in range(len(content)):
-        print content[n]
+    #for n in range(len(content)):
+        #print content[n]
 
       
 
     with open(str(page_num)+'-'+subject+".txt","wb") as content_file:
-        #print "content ="
-        #print content
+        ##print "content ="
+        ##print content
         content_file.write("\n".join(content).encode('utf-8'))
 
     
@@ -178,17 +178,17 @@ while True:
     
 
     local_next_URL = re.findall(r'href(.*?)>next<',html_file_text)
-    print "local_next_URL = "
-    print local_next_URL
-    #print html_file_text
-    print "URL 1 = " + local_next_URL[0]
+    #print "local_next_URL = "
+    #print local_next_URL
+    ##print html_file_text
+    #print "URL 1 = " + local_next_URL[0]
     local_next_URL = re.findall(r'=\"(.*?)\"',local_next_URL[0])
-    print "URL 2 =" + local_next_URL[0]
+    #print "URL 2 =" + local_next_URL[0]
     temp = local_next_URL[0].split('/')
     temp_temp = []
     
-    print "temp = "
-    print temp
+    #print "temp = "
+    #print temp
     for n in range(len(temp)):
         if temp[n]=='..':
             local_current_URL.pop()    
@@ -200,8 +200,8 @@ while True:
     if temp[-1] == "examples-hooves.php.htm":
         temp[-1] = "examples of hooves.php.htm"
 
-    print "local_current_URL = "
-    print local_current_URL
+    #print "local_current_URL = "
+    #print local_current_URL
     
     local_next_URL = local_current_URL
 
@@ -214,7 +214,7 @@ while True:
 
    
 
-    print "local_next_URL = " +  local_next_URL
+    #print "local_next_URL = " +  local_next_URL
     local_current_URL = local_next_URL
 
     if page_num == page_end:
@@ -223,7 +223,7 @@ while True:
 
     page_num = page_num + 1
     
-    print "page_num = " + str(page_num)
+    #print "page_num = " + str(page_num)
     
 cf.set('mission','progress',progress+1)
 cf.set('mission','URL',local_next_URL)
---
+++ adjust.ahk
@@ -0,0 +1,44 @@
+counter = 62
+hotkey RButton, dotheloop
+Esc::ExitApp
+return 
+
+dotheloop:
+
+loop , %counter%														
+{
+send {tab 14}
+Sleep 500
+send ^{a}
+Sleep 500
+send ^{x}
+Sleep 500
+send {f3}
+Sleep 500
+send ^{v}
+Sleep 500
+send {enter}
+Sleep 500*3
+send {tab 2}
+Sleep 500
+send ^{a}
+Sleep 500
+send ^{x}
+Sleep 500
+send ^{t}
+Sleep 500
+send {tab 2}
+Sleep 500
+send {enter}
+Sleep 500*20
+send +{tab 16}
+Sleep 500
+send {down}
+Sleep 500
+
+}
+
+ExitApp
+return 
+
+
